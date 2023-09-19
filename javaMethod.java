6316a9328ada3eb92a100017ce1a63b61d94b3b3
Kwazikonke <21707921@dut4life.ac.za>
2023-09-19 22:03:58 +0200

    Calling a method

---
+++ javaMethod.java
@@ -0,0 +1,13 @@
+package JavaDeepDive;
+
+public class javaMethod {
+
+	public static void greet() {
+		System.out.println("Hello World!");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+greet();
+	}
+
+}
